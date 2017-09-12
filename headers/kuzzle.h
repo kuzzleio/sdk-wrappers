37834d061a5e1c9defdef6d19091167fecc2f141
jenow <jenova.ke20@gmail.com>
2017-09-12 11:05:26 +0200

    add remove_listener

--- headers/kuzzle.h
+++ headers/kuzzle.h
@@ -234,5 +234,6 @@ extern void kuzzle_wrapper_query(kuzzle*, kuzzle_response*, kuzzle_request*, que
 extern void kuzzle_wrapper_set_headers(kuzzle*, json_object*, unsigned);
 extern json_object* kuzzle_wrapper_get_headers(kuzzle*);
 extern void kuzzle_wrapper_add_listener(kuzzle*, int, void*);
+extern void kuzzle_wrapper_remove_listener(kuzzle*, int);
 
 #endif
