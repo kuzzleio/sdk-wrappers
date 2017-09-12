3fca2aac6cee51af916d1773e9498c70444ab35e
David Bengsch <dbengsch@kaliop.com>
2017-09-12 16:58:30 +0200

    Comment out create which is not finished

--- cgo/kuzzle/collection_create.go
+++ cgo/kuzzle/collection_create.go
@@ -4,6 +4,7 @@ package main
 	#cgo CFLAGS: -I../../headers
 	#include <kuzzle.h>
 */
+/*
 import "C"
 import (
 	"unsafe"
@@ -13,40 +14,40 @@ import (
 
 //export kuzzle_wrapper_collection_create
 func kuzzle_wrapper_collection_create(c *C.collection, result *C.toto, options *C.query_options) {
-	var opts types.QueryOptions
-	if options != nil {
-		opts = SetOptions(options)
-	}
-
-	res, err := (*collection.Collection)(c.instance).Create(opts)
-	if err != nil {
-		result.error = *(*[2048]C.char)(unsafe.Pointer(C.CString(err.Error())))
-		return
-	}
-
-	res, err := KuzzleInstance.CheckToken(C.GoString(token))
-	if err != nil {
-		if err.Error() == "Kuzzle.CheckToken: token required" {
-			return C.int(C.EINVAL)
-		} else {
-			result.error = *(*[2048]C.char)(unsafe.Pointer(C.CString(err.Error())))
-			return 0
+		var opts types.QueryOptions
+		if options != nil {
+			opts = SetOptions(options)
 		}
-	}
 
-	var valid C.uint
+		res, err := (*collection.Collection)(c.instance).Create(opts)
+		if err != nil {
+			result.error = *(*[2048]C.char)(unsafe.Pointer(C.CString(err.Error())))
+			return
+		}
+		res, err := KuzzleInstance.CheckToken(C.GoString(token))
+		if err != nil {
+			if err.Error() == "Kuzzle.CheckToken: token required" {
+				return C.int(C.EINVAL)
+			} else {
+				result.error = *(*[2048]C.char)(unsafe.Pointer(C.CString(err.Error())))
+				return 0
+			}
+		}
 
-	if res.Valid {
-		valid = 1
-	} else {
-		valid = 0
-	}
+		var valid C.uint
 
-	*result = C.token_validity{
-		valid:     valid,
-		state:     *(*[512]C.char)(unsafe.Pointer(C.CString(res.State))),
-		expiresAt: C.int(res.ExpiresAt),
-	}
+		if res.Valid {
+			valid = 1
+		} else {
+			valid = 0
+		}
 
-	return 0
+		*result = C.token_validity{
+			valid:     valid,
+			state:     *(*[512]C.char)(unsafe.Pointer(C.CString(res.State))),
+			expiresAt: C.int(res.ExpiresAt),
+		}
+
+		return 0
 }
+*/
