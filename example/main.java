f3e4f3ed434f4ae1e573178a9f9a59bf073c0803
jenow <jenova.ke20@gmail.com>
2017-09-14 16:57:04 +0200

    wrap ctor for java

--- example/main.java
+++ example/main.java
@@ -4,24 +4,8 @@ import java.lang.reflect.Method;
 public class main {
     public static void main(String argv[]) {
         try {
-            kuzzle k = kcore.Kuzzle("localhost:7512", "websocket");
-
-            query_options options = new query_options();
-            options.setScroll("yolo");
-
-            try {
-
-                // System.out.println(k.login("local"));
-                // ack_response r = k.createMyCredentials("local", options);
-                r.getAcknowledged();
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-/*
-            System.out.println(k.checkToken("test").getValid());
-            System.out.println(k.checkToken("test").getState());
-            System.out.println(k.checkToken("test").getExpiresAt());
-*/
+            Kuzzle k = new Kuzzle("localhost:7512");
+            System.out.println(k.checkToken(null));
         } catch(Exception e) {
             e.printStackTrace();
         }
--- templates/kuzzle.i
+++ templates/kuzzle.i
@@ -16,4 +16,26 @@
     ~Kuzzle() {
         free($self);
     }
+
+    %exception checkToken {
+      $action
+      if (result == $null) {
+        jclass clazz = (*jenv)->FindClass(jenv, "java/lang/IllegalArgumentException");
+        (*jenv)->ThrowNew(jenv, clazz, "Kuzzle.CheckToken: token required");
+        return $null;
+      }
+    }
+    token_validity* checkToken(char* token) {
+        static token_validity res;
+        int err = kuzzle_wrapper_check_token($self, &res, token);
+
+        if (err == 0) {
+            return &res;
+        }
+        return (void*)0;
+    }
+
+    char* connect() {
+        kuzzle_wrapper_connect($self);
+    }
 }
