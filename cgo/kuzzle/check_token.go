832407258247c2ae9559e8000288370342f2424d
scottinet <scottinet@kuzzle.io>
2017-10-11 14:28:34 +0200

    pointer check

--- cgo/kuzzle/check_token.go
+++ cgo/kuzzle/check_token.go
@@ -12,8 +12,12 @@ import (
 
 //export kuzzle_wrapper_check_token
 func kuzzle_wrapper_check_token(k *C.Kuzzle, token *C.char) *C.token_validity {
-	res, err := (*kuzzle.Kuzzle)(k.instance).CheckToken(C.GoString(token))
 	result := (*C.token_validity)(C.calloc(1, C.sizeof_token_validity))
+
+	if result == nil:
+		return result
+
+	res, err := (*kuzzle.Kuzzle)(k.instance).CheckToken(C.GoString(token))
 
 	if err != nil {
 		result.error = C.CString(err.Error())
