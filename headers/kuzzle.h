aa9508b33fb1dfda3085f88d28dd1052f98f1885
David Bengsch <dbengsch@kaliop.com>
2017-10-26 15:01:30 +0200

    External definition

--- headers/kuzzle.h
+++ headers/kuzzle.h
@@ -400,6 +400,7 @@ extern json_result* kuzzle_wrapper_update_my_credentials(kuzzle*, char*, json_ob
 extern options* kuzzle_wrapper_new_options(void);
 
 //Json
+extern void kuzzle_wrapper_json_new(json_object**);
 extern void kuzzle_wrapper_json_put(json_object*, char*, void*, int);
 extern char* kuzzle_wrapper_json_get_string(json_object*, char*);
 extern int kuzzle_wrapper_json_get_int(json_object*, char*);
@@ -409,5 +410,44 @@ extern json_object* kuzzle_wrapper_json_get_json_object(json_object*, char*);
 
 //gc management
 extern void unregisterKuzzle(kuzzle*);
+
+//Collection
+extern collection* kuzzle_wrapper_new_collection(kuzzle*, char*, char*);
+extern ack_result* kuzzle_wrapper_collection_create(collection*, query_options*);
+extern bool_result* kuzzle_wrapper_collection_publish_message(collection*, json_object*, query_options*);
+extern void kuzzle_wrapper_collection_set_headers(collection*, json_object*, uint);
+extern ack_result* kuzzle_wrapper_collection_truncate(collection*, query_options*);
+
+//Collection Document
+extern int_result* kuzzle_wrapper_collection_count(collection*, search_filters*, query_options*);
+extern document_result* kuzzle_wrapper_collection_create_document(collection*, char*, document*, query_options*);
+extern string_result* kuzzle_wrapper_collection_delete_document(collection*, char*, query_options*);
+extern bool_result* kuzzle_wrapper_collection_document_exists(collection*, char*, query_options*);
+extern document_result* kuzzle_wrapper_collection_fetch_document(collection*, char*, query_options*);
+extern document_result* kuzzle_wrapper_collection_replace_document(collection*, char*, document*, query_options*);
+extern document_result* kuzzle_wrapper_collection_update_document(collection*, char*, document*, query_options*);
+extern search_result* kuzzle_wrapper_collection_scroll(collection*, char*, query_options*);
+extern search_result* kuzzle_wrapper_collection_search(collection*, search_filters*, query_options*);
+extern search_result* kuzzle_wrapper_collection_m_create_document(collection*, document**, uint, query_options*);
+extern search_result* kuzzle_wrapper_collection_m_create_or_replace_document(collection*, document**, uint, query_options*);
+extern string_array_result* kuzzle_wrapper_collection_m_delete_document(collection*, char**, uint, query_options*);
+extern search_result* kuzzle_wrapper_collection_m_get_document(collection*, char**, uint, query_options*);
+extern search_result* kuzzle_wrapper_collection_m_replace_document(collection*, document**, uint, query_options*);
+extern search_result* kuzzle_wrapper_collection_m_update_document(collection*, document**, uint, query_options*);
+
+//Collection Mapping
+extern mapping_result* kuzzle_wrapper_collection_get_mapping(collection*, query_options*);
+extern bool_result* kuzzle_wrapper_mapping_apply(mapping*, query_options*);
+extern bool_result* kuzzle_wrapper_mapping_refresh(mapping*, query_options*);
+extern void kuzzle_wrapper_mapping_set(mapping*, json_object*);
+extern void kuzzle_wrapper_mapping_set_headers(mapping*, json_object*, uint);
+
+//Collection Specification
+extern ack_result* kuzzle_wrapper_collection_delete_specifications(collection*, query_options*);
+extern specification_result* kuzzle_wrapper_collection_get_specifications(collection*, query_options*);
+extern specification_search_result* kuzzle_wrapper_collection_scroll_specifications(collection*, char*, query_options*);
+extern specification_search_result* kuzzle_wrapper_collection_search_specifications(collection*, search_filters*, query_options*);
+extern specification_result* kuzzle_wrapper_collection_update_specifications(collection*, specification*, query_options*);
+extern bool_result* kuzzle_wrapper_collection_validate_specifications(collection*, specification*, query_options*);
 
 #endif
