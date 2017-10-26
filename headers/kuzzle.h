931e9d9209f10f01575fd38db95a496341574192
jenow <jenova.ke20@gmail.com>
2017-10-26 15:43:34 +0200

    moved json wrapper prototype to swig.h

--- headers/kuzzle.h
+++ headers/kuzzle.h
@@ -518,16 +518,6 @@ extern json_result* kuzzle_wrapper_update_my_credentials(kuzzle*, char*, json_ob
 //Options
 extern options* kuzzle_wrapper_new_options();
 
-//Json
-extern void kuzzle_wrapper_json_new(json_object**);
-extern void kuzzle_wrapper_json_put(json_object*, char*, void*, int);
-extern char* kuzzle_wrapper_json_get_string(json_object*, char*);
-extern int kuzzle_wrapper_json_get_int(json_object*, char*);
-extern double kuzzle_wrapper_json_get_double(json_object*, char*);
-extern json_bool kuzzle_wrapper_json_get_bool(json_object*, char*);
-extern _json_object kuzzle_wrapper_json_get_json_object(json_object*, char*);
-extern void kuzzle_wrapper_json_new(json_object**);
-
 //gc management
 extern void unregisterKuzzle(kuzzle*);
 
